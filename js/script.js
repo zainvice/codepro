00c425d784455d026eb34e69bb4f9fb10882f6b0
Zain <amazwings@gmail.com>
2022-10-30 09:12:10 +0500

    New Update

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+function sayHello(){
+    var name=document.getElementById("name").value;
+    var msg= "Hello "+name+"!";
+    document.getElementById("response").textContent=msg;
+}
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
